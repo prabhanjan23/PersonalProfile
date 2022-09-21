a28292f70bbf036e3acdac6047bc3adfaf707ae1
prabhanjan23 <81773803+prabhanjan23@users.noreply.github.com>
2022-09-20 21:49:33 -0400

    Add files via upload

---
+++ Projects/Java/Calling Calling_Classes_and_Types_of_Lists/FIndingArea.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing_2;
+
+/**
+ *
+ * @author PrabhanjanPiwalatkar
+ */
+public class FIndingArea {
+    static int Area(int a){
+        return a*a;
+    }
+    static int Area(int a, int b){
+        return a*b;
+    }
+}
